a26c9144b7b90dbb15eb98bc66af966dfa871bb2
Ethan Hogan-Cloke <toga56784@gmail.com>
2025-08-14 10:40:43 +0100

    Started drawing triangle

--- ASCII3D/Geometry/triangle.py
+++ ASCII3D/Geometry/triangle.py
@@ -1,3 +1,11 @@
+from ..config import BRIGHTNESS
+
 class Triangle:
-    def __init__(self):
-        pass
+    def __init__(self, point_1, point_2, point_3):
+        self.point_1 = point_1
+        self.point_2 = point_2
+        self.point_3 = point_3
+    def draw(self, screen, brightness:int):
+        screen.draw_char(self.point_1[0], self.point_1[1], BRIGHTNESS[brightness])
+        screen.draw_char(self.point_2[0], self.point_2[1], BRIGHTNESS[brightness])
+        screen.draw_char(self.point_3[0], self.point_3[1], BRIGHTNESS[brightness])
